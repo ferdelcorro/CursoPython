930377490d2f166ce908e64ec5bd3e3b525e366c
Fernando del Corro <ferdelcorro@gmail.com>
2015-04-18 22:57:46 -0300

    correcciones

--- Simple_Socket/src/client.py
+++ Simple_Socket/src/client.py
@@ -4,43 +4,31 @@ from server.setting import *
 from socket import socket
 from client.tools import Recive, Envia
 
-HOST = 'localhost'
-PORT = 6032
+HOST = 'tuxis-us-03.tuxis.com.ar'
+PORT = 6030
 
 connected = []
 
+
 def main():
-    #Crea objeto socket
-    s = socket()
-    #Abre la conexion con el server
-    s.connect((HOST, PORT))
-    #Recibe informacion del server
-    r = Recive(s, connected)
-    e = Envia(s, connected)
-    r.start()
-    e.start()
-    """
-    input_data = s.recv(1024)
-    #Imprime la informacion recibida
-    print input_data
-    while True:
-        #Solicita ingreso de caracter
-        output_data = raw_input('> ')
-        if output_data:
-            try:
-                #Envia la informacion
-                s.send(output_data)
-            except TypeError:
-                #En caso de dar error por caracteres se hace una convercion
-                s.send(bytes(output_data, 'utf-8'))
-            #Recive mensaje del servidor
-            input_data = s.recv(1024)
-            #Imprime lo recibido
-            print input_data
-            #Si lo que enviamos es quit entonces cierra el programa
-            if output_data == '\quit':
-                return
-    """
+
+    try:
+        #Crea objeto socket
+        s = socket()
+        #Abre la conexion con el server
+        s.connect((HOST, PORT))
+        #Recibe informacion del server
+        r = Recive(s, connected)
+        e = Envia(s, connected)
+        r.start()
+        e.start()
+
+    except KeyboardInterrupt:
+        r.my_socket.close()
+        e.my_socket.close()
+
+        r.__stop = True
+        e.__stop = True
 
 
 if __name__ == "__main__":
